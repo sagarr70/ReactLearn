8614aab1938440f7548262008c9addad9270b29a
sagar aggarwal <sagaraggarwal@Sagars-MacBook-Air.local>
2025-11-26 01:42:25 +0530

    Begin with food app

--- app.js
+++ app.js
@@ -1,21 +1,614 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-//const heading = React.createElement('h1', { id: "heading" , xyz:"abc"} , 'Hello World!'); // props
-const dv = React.createElement('div', {id:"innerdiv"}, 
-    [
-    React.createElement('div', {id:"innerdiv1", key:"div1"}, 
-    [React.createElement('h1', { id: "heading1" , key:"h1"} , "Hello World!"),
-        React.createElement('h2', { id: "heading2" , key:"h2"} , 'Hello World 2!')]),
+const resDataList = [
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "53770",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/c0adbf21-5fba-47ec-94d4-4300f7ff7ed0_53770.JPG",
+      locality: "Lajpat Nagar - 2",
+      areaName: "Lajpat Nagar - 2",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.3,
+      parentId: "721",
+      avgRatingString: "4.3",
+      totalRatingsString: "9.2K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 1.3,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-11-26 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹99",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.4",
+          ratingCount: "1.9K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-4da2e3cd-b929-4925-a5b5-16cb88b6242c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/pizza-hut-lajpat-nagar-2-rest53770",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "24184",
+      name: "Domino's Pizza",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/11/11/50826a85-d04f-48e2-a1fa-8e353fb8ad6d_24184.JPG",
+      locality: "Kailash Colony",
+      areaName: "Greater Kailash 1",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.6,
+      parentId: "2456",
+      avgRatingString: "4.6",
+      totalRatingsString: "19K+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 2.3,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-11-26 03:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-4da2e3cd-b929-4925-a5b5-16cb88b6242c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/dominos-pizza-kailash-colony-greater-kailash-1-rest24184",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "376932",
+      name: "Ghar Ka Tadka",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/1/1f4794c4-eb55-401b-b2b5-0a26c69ee1a5_376932.jpg",
+      locality: "Zamrudpur",
+      areaName: "Amar Colony",
+      costForTwo: "₹150 for two",
+      cuisines: ["Indian"],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "8838",
+      avgRatingString: "4.2",
+      totalRatingsString: "5.0K+",
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 2.3,
+        serviceability: "SERVICEABLE",
+        slaString: "30-40 mins",
+        lastMileTravelString: "2.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-12-02 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "65% OFF",
+        subHeader: "UPTO ₹150",
+        discountTag: "SAVE BIG",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-4da2e3cd-b929-4925-a5b5-16cb88b6242c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/ghar-ka-tadka-zamrudpur-amar-colony-rest376932",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "45890",
+      name: "Faasos - Wraps, Rolls & Shawarma",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/28/c949c23a-0c7e-400b-8c25-405b97129620_45890.JPG",
+      locality: "Kotla Mubarak Pur",
+      areaName: "Defence Colony",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Kebabs",
+        "Fast Food",
+        "Snacks",
+        "American",
+        "Healthy Food",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.4,
+      parentId: "21809",
+      avgRatingString: "4.4",
+      totalRatingsString: "18K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 4.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "4.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-11-26 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Rolls.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Rolls.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-4da2e3cd-b929-4925-a5b5-16cb88b6242c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/faasos-wraps-rolls-and-shawarma-kotla-mubarak-pur-defence-colony-rest45890",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "697444",
+      name: "Mealy - Your Everyday Meal",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/5/a0f4c6de-2d65-4f2c-8f44-83da0eca8d11_697444.jpg",
+      locality: "Ground Floor",
+      areaName: "Greater Kailash 2",
+      costForTwo: "₹400 for two",
+      cuisines: ["Beverages", "Street Food"],
+      avgRating: 4.4,
+      parentId: "11705",
+      avgRatingString: "4.4",
+      totalRatingsString: "5.2K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 0.5,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "0.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-11-26 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-4da2e3cd-b929-4925-a5b5-16cb88b6242c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/mealy-your-everyday-meal-ground-floor-greater-kailash-2-rest697444",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "25180",
+      name: "BTW",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/7/8/6f56e90e-3cab-44d2-8de5-4aaf1d2e9861_25180.JPG",
+      locality: "Jasola",
+      areaName: "Jasola",
+      costForTwo: "₹300 for two",
+      cuisines: ["Street Food", "North Indian", "Sweets"],
+      avgRating: 4.1,
+      veg: true,
+      parentId: "10314",
+      avgRatingString: "4.1",
+      totalRatingsString: "34K+",
+      sla: {
+        deliveryTime: 41,
+        lastMileTravel: 8.4,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "8.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-11-26 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Ratnesh_Badges/Rx_Awards_2025/Tikki.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Ratnesh_Badges/Rx_Awards_2025/Tikki.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹42",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.1",
+          ratingCount: "8.4K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-4da2e3cd-b929-4925-a5b5-16cb88b6242c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/btw-jasola-rest25180",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
 
-    React.createElement('div', {id:"innerdiv2" , key:"div2"}, 
-    [React.createElement('h3', { id: "heading3" , key:"h3"} , "Hello World!"),
-        React.createElement('h4', { id: "heading4" , key:"h4"} , 'Hello World 2!')])
-    ]
-    
-    
-    );
+// Header Component
+const HeaderComponent = () => {
+  return (
+    <header className="app-header">
+      <div className="logo-container">
+        <img
+          src="https://img.freepik.com/premium-vector/food-ordering-app-logo-with-points-fork-shapes-center_666184-195.jpg?semt=ais_hybrid&w=740&q=80"
+          alt="App Logo"
+          className="app-logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+// Restaurant Card Component
+const RestaurantCardComponent = (props) => {
+  const { id, name, cuisine, rating, imageUrl, price } = props;
+  return (
+    <div className="restaurant-card" uniquekey={id}>
+      <img src={imageUrl} alt={name} className="restaurant-image" />
+      <div className="restaurant-info">
+        <h3 className="restaurant-name">{name}</h3>
+        <p className="restaurant-cuisine">{cuisine}</p>
+        <p className="restaurant-rating">{rating} ★</p>
+        <p className="restaurant-price">Price: {price}</p>
+      </div>
+    </div>
+  );
+};
 
-        
-root.render(dv);
+// Body Component
+const BodyComponent = () => {
+    return (
+    <div className="app-body">
+      <div className="search-bar">
+        <input
+          id="search-input"
+          type="text"
+          placeholder="Search for restaurants or cuisines..."
+        />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="restaurant-container">
+        {resDataList.map((restaurant) => (
+          <RestaurantCardComponent
+            key={restaurant.info.id}
+            id={restaurant.info.id}
+            name={restaurant.info.name}
+            cuisine={restaurant.info.cuisines.join(", ")}
+            rating={restaurant.info.avgRating}
+            price={restaurant.info.costForTwo}
+            imageUrl={
+              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+              restaurant.info.cloudinaryImageId
+            }
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+//Footer Component
+const FooterComponent = () => {
+  return (
+    <footer className="app-footer">
+      <p>© 2025 FoodieApp. All rights reserved.</p>
+    </footer>
+  );
+};
+
+// App Component Layout
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <BodyComponent />
+      <FooterComponent />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = ReactDom.createRoot(rootElement);
+
+root.render(<AppLayout />);
